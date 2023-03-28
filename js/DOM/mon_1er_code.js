1eb2eadb89b15ab2268c932b60547e508cd36392
27010-17-08 <27010-17-08@exchange.ad.afpanet>
2023-03-28 16:46:52 +0200

    save2803

---
+++ js/DOM/mon_1er_code.js
@@ -0,0 +1,43 @@
+/***************question 1********* *
+document.write("<h2>Hello World!</h2><p>Have a nice day!</p>");
+alert("<b>ceci est un test</b>");
+/******************question 2***** *
+var nomexterne="Hein";
+var prenom="Terreur";
+function portee(nom){
+    
+    nomglobale="Halle";
+    console.log(window.nomgolbale+nom+prenom);
+    console.log(nomglobale+nomexterne+prenom);
+}
+portee("ex ");
+console.log(prenom);
+/***********question 3************ *
+<form action="" id="form1" name="form1">
+        <input type="text" name="nom" value="Mozart" /> <input type="text" name="prenom" value="Ella" /> <input
+            type="submit" id="idSubForm1" name="subForm1" value="Connection" />
+    </form>
+    <div id="postit">
+        <button>cache</button>
+        <button>afficher</button>
+
+    </div>
+/****************question 4 *************/
+document.querySelector('p').textContent = '1er paragraphe du document';
+let documentDiv = document.querySelector('div');
+documentDiv.querySelector('p').textContent = '1er paragraphe du premier div';
+document.querySelector('p.bleu').style.color = 'blue';
+let documentParas = document.querySelectorAll('p');
+let divParas = documentDiv.querySelectorAll('p');
+documentParas.forEach(function(nom, index){
+    nom.textContent += ' (paragraphe n°:' + index + ')';
+});
+document.getElementById('p1').style.color = 'blue';
+let bleu = document.getElementsByClassName('bleu');
+for(valeur of bleu){
+    valeur.style.color = 'blue';
+}
+let paras = document.getElementsByTagName('p');
+for(valeur of paras){
+    valeur.style.color = 'blue';
+}
--- js/exop8fct/jav.js
+++ js/exop8fct/jav.js
@@ -62,18 +62,84 @@ function tablemulti(x){
 }
 alert("Coucou !");
 console.log("Ah que Coucou !");
-document.write("Coucou beuh !");*/
+document.write("Coucou beuh !");
 var elt1 = document.querySelector(".example");
 var bobo = document.querySelector("body");
-console.log("La 2nde zone de texte a pour nom :"); 
+console.log("La 2nde zone de texte a pour nom :");
 console.log(document.forms['form1'].elements[1].name);
 console.log("La 1ère zone de texte a pour valeur :'");
 console.log(document.forms['form1'].elements['nom'].value);
 console.log("Le bouton Submit a pour identifiant unique : ");
 console.log(document.getElementById('idSubForm1').id)
-;
-console.log("Le bouton Submit a pour valeur :"); 
+    ;
+console.log("Le bouton Submit a pour valeur :");
 console.log(document.getElementsByName('subForm1')[0].value);
-document.getElementById('idDiv').innerHTML ="<h3>Titre</h3>";
+document.getElementById('idDiv').innerHTML = "<h3>Titre</h3>";
+
 console.log(elt1);
 console.log(bobo);
+
+function testerRadio() {
+    var radio = document.getElementsByName("btnRadChoix");
+    for (var i = 0; i < radio.length; i++) {
+        if (radio[i].checked) {
+            document.getElementById("txtBox1").value = radio[i].value;
+        }
+    }
+}
+var
+    version = navigator.appVersion; console.log("Lecode name de votre browser est :"
+        + navigator.appCodeName);
+console.log("Le nom ou la marque du browser est :"
+    + navigator.appName);
+console.log("Les informations sur la version sont:" + version);
+console.log("Le browser a comme user-agent :"
+    + navigator.userAgent);
+console.log("");
+if (version.indexOf('Win') > -1) {
+    console.log("<br />Vous etes sous l'environnement Windows");
+    console.log("<br />");
+}
+if (navigator.userAgent.indexOf('Firefox') > -1) {
+    console.log("Vous utilisez un navigateur FireFox");
+} 
+else if (version.indexOf('Chrome') > -1) {
+    console.log("Vous utilisez un navigateurChrome");
+} 
+else if (version.indexOf('MSIE') > -1) {
+    console.log("Vous utilisez un navigateur Microsoft Internet Explorer");
+} 
+else {
+    console.log("Vous utilisez un navigateur inconnu");
+}
+let b=document.body;
+let p1=document.getElementById('p1');
+let p2=document.getElementById('p2');
+let p4=b.lastElementChild;
+let newp=document.createElement('p');
+let newtext=document.createTextNode('texte ecris en js avec appenchild()')
+let clonep1=p1.cloneNode(true);
+let eltdel=b.removeChild(p1);
+p2.remove()
+alert('Noeud supprimé du dom: '+eltdel+'\nContenue :'+eltdel.textContent);
+p2.insertAdjacentElement('afterend',clonep1);
+/*newp.textContent='paragraphe crée grace au js avec appen()';
+b.insertBefore(p1,p4);
+b.append(newp,'text inséré avec append()');
+b.appendChild(newtext);
+p1.insertAdjacentElement('afterend',newp);
+p1.insertAdjacentHTML('beforeend',htmlContent);
+p2.insertAdjacentText('afterbegin','text ajouté dans');*/
+let p1 = document.querySelector('p');
+let p2 =document.getElementById('p2');
+let vide = document.getElementById('vide');
+
+p2.setAttribute('class','blue')
+vide.innerHTML+='class'+p1.className+'<br>id: '+p1.
+if (p1.hasAttribute()) {
+    let attp1 = p1.attributes;
+    vide.innerHTML = 'liste des attributs de p1 : <br>'
+    for (let i = 0; 0 < attp1.length; i++) {
+        vide.innerHTML += attp1[i].name + '=' + attp1[i].value + '<br>';
+    }
+}
