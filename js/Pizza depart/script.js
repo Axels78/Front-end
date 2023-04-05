5fed05297ded6abb708cb13d768217f02703d061
27010-17-08 <27010-17-08@exchange.ad.afpanet>
2023-04-05 16:44:18 +0200

    save0504

---
+++ js/Pizza depart/script.js
@@ -0,0 +1,26 @@
+var btn = document.getElementById('btn');
+btn.addEventListener('click', ChargeInfosJson());
+
+function ChargeInfosJson() {
+    fetch('pizza.json')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            createdivs(data);
+        });
+}
+function createdivs(_data) {
+    var preview=document.getElementsByClassName('preview')[0];
+    preview.innerHTML="";
+    var pizzerianame=document.createElement("div");
+    pizzerianame.innerHTML=_data.name;
+    pizzerianame.setAttribute('id','nompizza')
+    var pizzeraslogan=document.createElement("div");
+    pizzeraslogan.innerHTML=_data.slogan;
+    pizzeraslogan.setAttribute('id','slogan');
+
+    preview.appendChild(pizzerianame);
+    preview.appendChild(pizzeraslogan);
+}
