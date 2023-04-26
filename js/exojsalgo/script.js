13ecd75c52f2afa2f5618433c404093f92bd7a2c
27010-17-08 <27010-17-08@exchange.ad.afpanet>
2023-04-26 16:53:08 +0200

    todaytry

---
+++ js/exojsalgo/script.js
@@ -0,0 +1,10 @@
+let num=prompt("saisir un nombre :");
+function posneg(num){
+    if(num>0){
+        alert("nombre positif")
+    }
+    else{
+        alert("nombre négatif")
+    }
+}
+posneg(num);
